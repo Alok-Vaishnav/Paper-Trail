e72b7ccf6cd186293f52f1fcf2822d7eab5fcce9
Alok vaishnav <145538711+Alok-Vaishnav@users.noreply.github.com>
2025-11-12 21:40:06 +0530

    fix

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 connectToMogo();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
